c09bfbf9eea0f68dd55d8777cc59e85b78f1f769
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-28 08:38:00 -0700

    Implement half of the specs in specs.js

---
+++ specs/specs.js
@@ -0,0 +1,9 @@
+//1. User can input value for their Earth age in an input field;
+//2. User can then from a drop down menu select a planet to convert their age to the selected planets age;
+//3. From there the user can click sumbit, and .click function will take in the users input via .val() method;
+//4. The planet age converter will be in the form of if/else statements applying math operators to the users age based selected planet;
+// If the user selects the planet Mercury returns their age in Mercury years. (A Mercury year is .24 Earth years.) The following math would occur in the if/else statement: userInput * .24 = return;
+// If the user selects the planet Venus returns their age in Venus years. (A Venus year is .62 Earth years.) The following math would occur in the if/else statement: userInput * .62 = return;
+// If the user selects the planet Mars returns their age in Mars years. (A Mars year is 1.88 Earth years.)  The following math would occur in the if/else statement: userInput * 1.88 = return;
+// If the user selects the planet Jupiter returns their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)  The following math would occur in the if/else statement: userInput * 11.86 = return;
+//5.
