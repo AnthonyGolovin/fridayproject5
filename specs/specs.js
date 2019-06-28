8d96ca01a83f7bbc9d02d7ad653d82a87c641154
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-28 09:56:22 -0700

    Add front end javascript functionality

--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,6 @@
 //1. User can input value for their Earth birthday in the input field; X
-//2. User can then from a drop down menu select a planet to convert their age to the selected planets age;
-//3. From there the user can click sumbit, and .click function will take in the users input via .val() method;
+//2. User can then from a drop down menu select a planet to convert their age to the selected planets age; X
+//3. From there the user can click sumbit, and .click function will take in the users input via .val() method; X
 //4. The planet age converter will be in the form of if/else statements apply math operators to the users age birthday based the selected planet;
 // If the user selects the planet Mercury returns their age in Mercury years. (A Mercury year is .24 Earth years.) The following math would occur in the if/else statement: Todaysdate (via getFullYear()) - Date of birth * .24 = return;
 // If the user selects the planet Venus returns their age in Venus years. (A Venus year is .62 Earth years.) The following math would occur in the if/else statement: Todaysdate (via getFullYear()) - Date of birth * .62 = return;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-
 import './styles.css';
-
-$(function() {
-
+import $ from 'jquery';
+import 'bootstrap';
+import './styles.css';
+import './styles.css';
+$(document).ready(function() {
+  $("#calculate").click(function(event){
+    event.preventDefault();
+    let userDateOfBirth = $("#birthDayYear").val();
+  $("output").text(());
+  });
 });
