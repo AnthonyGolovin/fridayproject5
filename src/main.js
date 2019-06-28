d01de1f837393fca80e590ffe3d186cbd763f3c1
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-28 11:37:38 -0700

    Did more research and still couldnt find out what was going on so for now working on the scripting to make sure the program works before more research

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import './styles.css';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AgeCalculator } from './fridayproject5.js';
+// import './styles.css';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { AgeCalculator } from './fridayproject5.js';
 
 //front-end
 $(document).ready(function() {
+console.log("Hello World");
+console.log(Date());
 
   $("#calculate").click(function(event){
     event.preventDefault();
-    // let userDateOfBirth = $("#birthDayYear").val();
-    // AgeCalculator();
-    // console.log(getFullYear);
-    // console.log(beb);
-    console.log(AgeCalculator());
+    let userDateOfBirth = $("#birthDayYear").val();
+    let year = new Date().getFullYear();
+    console.log(year);
     console.log("Hello World");
-  // $("output").text(userDateOfBirth);
+  
+  $("output").text(userDateOfBirth);
   });
+
 });
