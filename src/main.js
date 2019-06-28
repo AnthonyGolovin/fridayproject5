89515179025101fb9084f47e84b3ee9e8ffcfba1
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-28 10:59:25 -0700

    index.html has not been detecting anything I try to import, so I might just have to and look into it later

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AgeCalculator } from './fridayproject5.js';
-import './styles.css';
 
 //front-end
 $(document).ready(function() {
 
   $("#calculate").click(function(event){
     event.preventDefault();
-    let userDateOfBirth = $("#birthDayYear").val();
+    // let userDateOfBirth = $("#birthDayYear").val();
     // AgeCalculator();
     // console.log(getFullYear);
     // console.log(beb);
     console.log(AgeCalculator());
     console.log("Hello World");
-  // $("output").text(());
+  // $("output").text(userDateOfBirth);
   });
 });
