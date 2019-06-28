e2300b4253458cd904eecde046f659311d4f20b8
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-28 12:46:09 -0700

    Add all the the base functions needed for both planet age calculation and life expectancy, going to investigate the karma and input issues

--- specs/specs.js
+++ specs/specs.js
@@ -7,5 +7,5 @@
 // If the user selects the planet Mars returns their age in Mars years. (A Mars year is 1.88 Earth years.)  The following math would occur in the if/else statement: Todaysdate (via getFullYear()) - Date of birth * 1.88 = return;
 // If the user selects the planet Jupiter returns their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)  The following math would occur in the if/else statement: Todaysdate (via getFullYear()) - Date of birth * 11.86 = return;
 //5. Once the age is calculated there will be a second aspect to the if/else statement that assumes everyones life expectancy is 80 years old.
-//6. From there 80 would be multiplied in the function by the planets unique year (EX. 80 * .24 = 19.2) and it would take the product and subtract it from the users planet age. (EX. 19.02(lifeExpectancy) - 13.02(myPlanetAge)= specified planets life expectancy.)
+//6. From there 80 would be multiplied in the function by the planets unique year (EX. 80 * .24 = 19.2) and it would take the product and subtract it from the users planet age. (EX. 49.6(lifeExpectancy) - 13.02(myPlanetAge)= specified planets life expectancy.)
 //7. The results would be spitted back to the user in a <p> tag and then everyone is happy.
--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ console.log("Hello World");
 console.log(Date());
 let year = new Date().getFullYear();
 let answer = "";
+let lifeExpectancy = "";
+let planetLifeExpectancy = "";
+let averageLifeExpectancy = 80;
+
 // let mercury = $("option:value[1]")
 
   $("#calculate").click(function(event){
@@ -18,19 +22,28 @@ let answer = "";
     let year = new Date().getFullYear();
     if ($("#planet :selected").val() == "Mercury") {
        answer = (year - userDateOfBirth) * .24;
+       planetLifeExpectancy = averageLifeExpectancy * .24;
+      lifeExpectancy = planetLifeExpectancy - answer;
     } else if ($("#planet :selected").val() == "Venus") {
        answer = (year - userDateOfBirth) * .62;
+       planetLifeExpectancy = averageLifeExpectancy * .62;
+      lifeExpectancy = planetLifeExpectancy - answer;
     } else if ($("#planet :selected").val() == "Mars") {
        answer = (year - userDateOfBirth) * 1.88;
+       planetLifeExpectancy = averageLifeExpectancy * 1.88;
+      lifeExpectancy = planetLifeExpectancy - answer;
     } else if ($("#planet :selected").val() == "Jupiter") {
        answer = (year - userDateOfBirth) * 11.86;
-          }
+       planetLifeExpectancy = averageLifeExpectancy * 11.86;
+      lifeExpectancy = planetLifeExpectancy - answer;
+    }
 
 console.log(year - 2000);
 console.log($("#planet :selected").val());
 console.log(answer);
           // console.log(answer);
-  $("#output").text(answer);
+  $("#output").text("You are " answer + " years old on the planet of " + $("#planet :selected").val());
+  $("#output2").text(lifeExpectancy + " Is the average human life expectancy on the planet of " + $("#planet :selected").val());
   });
 // console.log(year - 2000);
 });
