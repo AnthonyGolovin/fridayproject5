136cc30b6c3ea567c347da5fec9366bef21ee1a4
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-28 13:59:18 -0700

    Oh my god I got karma to work

--- src/fridayproject5.js
+++ src/fridayproject5.js
@@ -1,6 +1,6 @@
-// 
-// export function AgeCalculator() {
-// let currentYear = new getFullYear(year);
-// let beb = 5;
-// return beb;
-// }
+
+export function AgeCalculator() {
+let currentYear = new getFullYear(year);
+let beb = 5;
+return beb;
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-// import './styles.css';
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { AgeCalculator } from './fridayproject5.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AgeCalculator } from './fridayproject5.js';
 
 //front-end
 $(document).ready(function() {
