450ca6d11243dbacfcf0d547106a3d77c2b41bc7
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-28 12:15:42 -0700

    Nearly finished the main functions creating framework for life expectancy

--- src/main.js
+++ src/main.js
@@ -8,15 +8,27 @@
 $(document).ready(function() {
 console.log("Hello World");
 console.log(Date());
+let year = new Date().getFullYear();
+// let mercury = $("option:value[1]")
 
   $("#calculate").click(function(event){
     event.preventDefault();
-    let userDateOfBirth = $("#birthDayYear").val();
+    let userDateOfBirth = parseInt($("#birthDayYear").val());
     let year = new Date().getFullYear();
-    console.log(year);
-    console.log("Hello World");
-  
-  $("output").text(userDateOfBirth);
+    if ($("#planet option:selected").val().equals("Mercury") == true) {
+      let answer = userDateOfBirth * .24 - year;
+    } else if ($("#planet :selected").val() == "Venus") {
+      let answer = userDateOfBirth * .62 - year;
+    } else if ($("#planet :selected").val() == "Mars") {
+      let answer = userDateOfBirth * 1.88 - year;
+    } else if ($("#planet :selected").val() == "Jupiter") {
+      let answer = userDateOfBirth * 11.86 - year;
+          }
+console.log(year - 2000);
+console.log($("#planet :selected").val());
+console.log(answer);
+          // console.log(answer);
+  $("output").text();
   });
-
+// console.log(year - 2000);
 });
